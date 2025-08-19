46a426994c0ff6ecb68e9834031df232b7dd53df
Alexandre  Erdmann <erdmannsilva@hotmail.com>
2025-08-18 21:50:49 -0300

    Atualizado o arquivo dashboard.php

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
-require_once("db.php");
-require_once("globals.php");
+
+  require_once("templates/header.php");
+
+  require_once("dao/MovieDAO.php");
+
+  // Dao dos Filmes
+  $movieDao = new MovieDAO($conn, $BASE_URL);
+
+  $lastestMovies = $movieDao->getLatestMovies();
+
+  $actionMovies = $movieDao->getMoviesByCategory("Ação");
+
+  $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
+
+
+  include_once("templates/footer.php");
+
 ?>
-<!DOCTYPE html>
-<head>
-<title>Filmes</title>
-</head>
-<body>
-    <h1>Conexão</h1>
-</body>
